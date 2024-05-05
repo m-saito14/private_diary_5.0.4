1f15042386395f36e3d75da7c87444b2a1dfd810
saitomtk <motoki.3110.1991@gmail.com>
2024-05-05 23:44:33 +0900

    edit diary_list.html

--- diary/urls.py
+++ diary/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
     path('diary-list/', views.DiaryListView.as_view(), name="diary_list"),
     path('dialy-list/<int:pk>/', views.DiaryDetailView.as_view(), name="dairy_detail"),
-    path('diary-create', views.DiaryCreateView.as_view(), name="dairy_create"),
+    path('diary-create', views.DiaryCreateView.as_view(), name="diary_create"),
     path('diary-update/<int:pk>/', views.DiaryUpdateView.as_view(), name = "diary_update"),
     path('diary-delete/<int:pk>/', views.DiaryDeleteView.as_view(), name="diary_delete"),
 ]
